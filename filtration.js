f6425c78bbf63e70c0117d856a0462ea8918615b
Loïc Séguin-C <lsc@loicseguin.com>
2017-12-08 13:19:28 -0500

    Refactor sensitometric functions into separate file

--- filtration.js
+++ filtration.js
@@ -80,35 +80,3 @@ function anodeFiltration(spectrum, depth, anodeAngle, beamAngle) {
     var spec = beerLambert(spectrum, thicknessCenter, muCoeffs);
     return [spec, energies];
 }
-
-function energyToDO(energy, gamma, Emax, DOmax) {
-    // Convert a given energy to an optical density. The film/numeric receptor
-    // is considered to be calibrated such that the maximum energy `Emax`
-    // corresponds to an optical density `DOmax`.
-    var x = Math.log(energy);
-    var DO = DOmax + (x - Math.log(Emax)) * gamma;
-    DO = d3.max([DO, 0.3]);
-    return DO;
-}
-
-function DOToRGB(optical_density) {
-    // Convert an optical density to an RGB value. 255 corresponds to white
-    // (minimum optical density, 0.3) whereas 0 corresponds to black (maximum
-    // optical density, 3.0).
-    var rgbMin = 0;
-    var rgbMax = 255;
-    var doMin = 0.3;
-    var doMax = 3;
-    return (optical_density - doMax) * (rgbMax - rgbMin) / (doMin - doMax);
-}
-
-function getDOs(energies, DOmax, gamma, Emax) {
-    // Draw a rectangle with grayscale corresponding to the given energies.
-    if (DOmax === undefined) { DOmax = 3.0; }
-    if (gamma === undefined) { gamma = 3.0; }
-    if (Emax === undefined) { Emax = d3.max(energies); }
-    var DOs = energies.map(function(d) {
-        return DOToRGB(energyToDO(d, gamma, Emax, DOmax));
-    });
-    return DOs;
-}
